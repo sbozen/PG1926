a8c2df17a80e7563af9f3bddebf685001075dffb
sbozen <sbozenn@gmail.com>
2021-02-08 23:09:30 +0300

    PG1926

---
+++ PG1926/php/perfectNumber/perfectNumb.php
@@ -0,0 +1,16 @@
+<?
+	$num = $_POST['number'];
+	$sum=0;
+	for ($i=0; $i <$num ; $i++) { 
+		
+		if ($num%$i==0) {
+			$sum+=$i
+			if ($sum==$num) {
+				$msg= "<script>alert('This number a perfect number')</script>";
+				echo "msg";
+			}
+
+		}
+	}
+
+?>
